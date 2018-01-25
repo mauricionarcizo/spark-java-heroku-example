26fbb974451c01efa6ebcd4411fee1fe91d82c75
unknown <mauricio.narcizo@handit.com.br>
2018-01-25 14:07:24 -0200

    commit initial

---
+++ src/main/java/spark/java/heroku/example/SparkJavaHerokuExample/App.java
@@ -0,0 +1,24 @@
+package spark.java.heroku.example.SparkJavaHerokuExample;
+
+import static spark.Spark.get;
+import static spark.SparkBase.port;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+	public static void main(final String[] args) {
+		port(getHerokuAssignedPort());
+		get("/hello", (req, res) -> "Hello Heroku World");
+	}
+
+	static int getHerokuAssignedPort() {
+		final ProcessBuilder processBuilder = new ProcessBuilder();
+		if (processBuilder.environment().get("PORT") != null) {
+			return Integer.parseInt(processBuilder.environment().get("PORT"));
+		}
+		return 4567; // return default port if heroku-port isn't set (i.e. on localhost)
+	}
+}
